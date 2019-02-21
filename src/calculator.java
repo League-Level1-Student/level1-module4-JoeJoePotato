8a2b56d24a9bb41996651f5f20c12ddac16536a4
JoeJoePotato <league@WTS-IM-17.attlocal.net>
2019-02-20 18:26:56 -0800

    calculator and typing tutor

--- src/calculator.java
+++ src/calculator.java
@@ -11,7 +11,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.URL;
 
-
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -21,24 +20,95 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import sun.awt.AWTAccessor.SystemTrayAccessor;
 
-public class calculator {
-JFrame frame=new JFrame();
-JLabel label= new JLabel();
-JButton add=new JButton("Add");
-JButton sub=new JButton("Subtract");
-JButton mult=new JButton("Multiply");
-JButton divi=new JButton("Divide");
+public class calculator implements MouseListener {
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add");
+	JButton sub = new JButton("Subtract");
+	JButton mult = new JButton("Multiply");
+	JButton divi = new JButton("Divide");
+	JTextField na = new JTextField();
+	JTextField nb = new JTextField();
+
+	int a = 0;
+	int b = 0;
 
 	public static void main(String[] args) {
-	calculator calc=new calculator();
-	calc.format();
+		calculator calc = new calculator();
+		calc.format();
 	}
+
 	public void format() {
 		frame.setVisible(true);
-		frame.add(label);
-label.add(add);
+
+		label.setText(
+				"enter two numbers and then select \nwhich operation you want to be performed.\nThen your answer will appear here.");
+		frame.add(panel);
+		panel.add(label);
+		add.addMouseListener(this);
+		sub.addMouseListener(this);
+		mult.addMouseListener(this);
+		divi.addMouseListener(this);
+
+		panel.add(na);
+
+		panel.add(nb);
+		panel.add(add);
+		panel.add(sub);
+		panel.add(mult);
+		panel.add(divi);
+frame.pack();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		String numa = na.getText();
+		String numb = nb.getText();
+		a = Integer.parseInt(numa);
+		b = Integer.parseInt(numb);
+		if (e.getSource() == add) {
+			int c = a + b;
+
+			label.setText(c + "");
+		} else if (e.getSource() == sub) {
+			int c = a - b;
+			label.setText(c + "");
+		} else if (e.getSource() == mult) {
+			int c = a * b;
+			label.setText(c + "");
+		} else if (e.getSource() == divi) {
+			int c = a / b;
+			label.setText(c + "");
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 }
---
+++ src/typingTutor.java
@@ -0,0 +1,91 @@
+
+import java.applet.AudioClip;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
+import java.util.Date;
+import java.util.Random;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import sun.awt.AWTAccessor.SystemTrayAccessor;
+
+public class typingTutor implements KeyListener {
+	char currentLetter;
+	JLabel label=new JLabel();
+int lives=5;
+	JFrame frame=new JFrame("TypeQuest");
+	int score=0;
+	public static void main(String[] args) {
+	typingTutor t=new typingTutor();
+	t.setup();
+}
+	void setup() {
+		frame.setVisible(true);
+		currentLetter = generateRandomLetter();
+		label.setFont(label.getFont().deriveFont(36.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		frame.addKeyListener(this);
+		frame.add(label);
+		frame.pack();
+	}
+ char generateRandomLetter() {
+      Random r = new Random();
+      return (char) (r.nextInt(26) + 'a');
+}
+void newLetter() {
+	 currentLetter=generateRandomLetter();
+	 label.setText(currentLetter+"");
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	char keyCode=e.getKeyChar();
+
+	if(keyCode==currentLetter) {
+	score+=1;
+	System.out.println("you typed "+currentLetter);
+
+	if(score>49) {		
+	System.out.println("You have succeded! The kingdom of Tipyng will revere you as a hero forever.");
+	}
+	newLetter();
+	}else {
+		lives-=1;
+		System.out.println("you typed "+e+" but you should've typed "+currentLetter);
+	if(lives<1) {
+		System.out.println("You have lost, please try again next time.");
+	System.exit(0);
+	}
+	newLetter();
+	}
+
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
