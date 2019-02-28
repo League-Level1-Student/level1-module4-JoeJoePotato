d65c77b6d861f0a3342928f3dd3e9398bd15948d
JoeJoePotato <league@WTS5.attlocal.net>
2019-02-27 17:34:19 -0800

    part of search twitter

---
+++ src/searhTweets.java
@@ -0,0 +1,97 @@
+
+import java.applet.AudioClip;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
+import java.util.Date;
+import java.util.Random;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import sun.awt.AWTAccessor.SystemTrayAccessor;
+
+public class searhTweets implements MouseListener {
+JButton searcher=new JButton();
+JTextField entry=new JTextField();
+JFrame frame=new JFrame();
+JPanel panel= new JPanel();
+public static void main(String[] args) {
+searhTweets t=new searhTweets();
+t.setup();
+}
+void setup() {
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(searcher);
+	panel.add(entry);
+	frame.pack();
+	searcher.addMouseListener(this);
+}
+private String getLatestTweet(String searchingFor) {
+
+    Twitter twitter = new TwitterFactory().getInstance();
+
+    AccessToken accessToken = new AccessToken(
+          "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+          "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+
+    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+          "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+
+    twitter.setOAuthAccessToken(accessToken);
+
+    Query query = new Query(searchingFor);
+    try {
+          QueryResult result = twitter.search(query);
+          return result.getTweets().get(0).getText();
+    } catch (Exception e) {
+          System.err.print(e.getMessage());
+          return "What the heck is that?";
+    }
+}
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	String a=entry.getText();
+	String b=getLatestTweet(a); 
+	System.out.println(b);
+}
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
